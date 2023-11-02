ff499e7dde13ded46784afa6106dbe7ee5340d3a
Matt Straathof <matt.straat@gmail.com>
2023-11-02 11:48:59 -0700

    feat: add example deployments with serverless and cdk

---
+++ cdk/lib/lambda-stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import * as path from "path";
+
+export class LambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const usersLambda = new lambda.Function(this, 'users-lambda', {
+      handler:'handler.handler',
+      code: lambda.Code.fromAsset(
+          path.join(
+              '..',
+              'dist',
+              'users',
+              'users.zip'
+          )
+      ),
+      runtime: lambda.Runtime.NODEJS_18_X,
+      functionName: 'users-cdk-boilerplate'
+    });
+
+    const productsLambda = new lambda.Function(this, 'products-lambda', {
+      handler:'handler.handler',
+      code: lambda.Code.fromAsset(
+          path.join(
+              '..',
+              'dist',
+              'products',
+              'products.zip'
+          )
+      ),
+      runtime: lambda.Runtime.NODEJS_18_X,
+      functionName: 'products-cdk-boilerplate'
+    });
+  }
+}
--- src/users/handler.ts
+++ src/users/handler.ts
@@ -1,11 +1,11 @@
 import {Context} from "aws-lambda";
 import log, {initLogger} from '../common/logger';
 
-export const handler = (event: unknown, context: Context) => {
+export const handler = async (event: unknown, context: Context) => {
     initLogger({
         lambdaContext: context,
         additionalContext: {},
     });
     log.info('Beginning users lambda execution');
-    return { statusCode: 200, body: JSON.stringify({ result: 'success', users: [] })}
+    return { statusCode: 200, body: 'Hello from users lambda' }
 }
